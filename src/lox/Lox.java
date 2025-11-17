6580d874978a8f72e081480e52d4759c4b571c47
tolgapasin <tolgapasin@gmail.com>
2025-11-17 17:21:58 +1000

    Finishing up basic lexer

--- src/lox/Lox.java
+++ src/lox/Lox.java
@@ -11,6 +11,8 @@ import java.nio.file.Paths;
 import java.util.List;
 
 public class Lox {
+    static boolean hadError = false;
+
     public static void main(String[] args) throws IOException {
         if (args.length > 1) {
             System.out.println("Usage: jlox [script]");
@@ -25,6 +27,9 @@ public class Lox {
     private static void runFile(String path) throws IOException {
         byte[] bytes = Files.readAllBytes(Paths.get(path));
         run(new String(bytes, Charset.defaultCharset()));
+
+        // Indicate an error in the exit code.
+        if (hadError) System.exit(65);
     }
 
     private static void runPrompt() throws IOException {
@@ -34,16 +39,28 @@ public class Lox {
         for (; ; ) {
             System.out.print("> ");
             String line = reader.readLine();
+            if (line == null) break;
+            run(line);
+            hadError = false;
         }
     }
 
     private static void run(String source) {
-        Scanner scanner = new Scanner(source);
-        List<Token> tokens = scanner.ScanTokens();
+        Lexer lexer = new Lexer(source);
+        List<Token> tokens = lexer.lexTokens();
 
         // For now, just print the tokens.
         for (Token token : tokens) {
             System.out.println(token);
         }
+    }
+
+    static void error(int line, String message) {
+        report(line, "", message);
+    }
+
+    private static void report(int line, String where, String message) {
+        System.err.println("[line " + line + "] Error" + where + ": " + message);
+        hadError = true;
     }
 }
